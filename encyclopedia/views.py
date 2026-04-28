92e2207204e524e5ed7db8eadbfc162ddb480cfd
aymen. <ayserraou@gmail.com>
2026-04-28 17:54:32 +0200

    feat : Entry page task completed

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def article(request, name):
+    return render(request,"encyclopedia/article.html",{
+        "entry" : util.get_entry(name),
+        "name" : name
+    })
