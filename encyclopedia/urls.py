e843fbee5f0472396416ff98d7083bbbd61582fe
aymen. <ayserraou@gmail.com>
2026-05-08 00:56:39 +0200

    feat : Search completed

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
 from . import views
 
-urlpatterns = [
+app_name = "encyclopedia"
+urlpatterns =[
     path("", views.index, name="index"),
     path("<str:name>", views.article, name="article")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-
 from . import util
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 
 
 def index(request):
+    if request.method == "POST":
+        search = request.POST.get("q")
+        if search:
+            if util.get_entry(search) or any(search in s for s in util.list_entries()) :
+                matching = [s for s in util.list_entries() if search in s]
+                return HttpResponseRedirect(reverse("encyclopedia:article", args=[matching[0]]))
+            else:
+                return render(request, "encyclopedia/index.html", {
+                    "entries": util.list_entries(),
+                    "error": "Article not found"
+                })
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries()
+            })
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
@@ -12,4 +29,4 @@ def article(request, name):
     return render(request,"encyclopedia/article.html",{
         "entry" : util.get_entry(name),
         "name" : name
-    })
+    }) 
